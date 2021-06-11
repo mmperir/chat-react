a7362151c376a86fffc065a06c431b248b9a2807
Marcelo Mendonca <marcelomendonca223@gmail.com>
2021-06-11 11:39:44 -0300

    Sistema base funcionando

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,59 +1,76 @@
-import {
-  Avatar,
-  Button,
-  Container,
-  TextField,
-  Typography,
-} from "@material-ui/core";
+import { Button, Container, TextField, Typography } from "@material-ui/core";
 import { FC, useState } from "react";
+import { FirebaseAuth, FirebaseFirestore } from "../../services/firebase";
 
 const SignUp: FC = () => {
-  const [nome, setNome] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-  const [confirmarSenha, setConfirmarSenha] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-  function onSubmit() {
-    console.log({ nome, email, senha, confirmarSenha });
+  async function onSubmit() {
+    try {
+      if (password !== confirmPassword) {
+        window.alert("Senhas não coinsidem");
+      }
+
+      const credential = await FirebaseAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      const uid = credential.user?.uid;
+
+      FirebaseFirestore.collection("users").doc(uid).set({ name });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
     <Container maxWidth="sm">
       <Typography variant="h4">Cadastro de usuário</Typography>
 
-      <Avatar src="http://github.com/mayconsgs.png" />
-
-      <TextField
-        onChange={(e) => setNome(e.target.value)}
-        margin="normal"
-        label="Nome"
-        fullWidth
-        variant="outlined"
-      />
-      <TextField
-        onChange={(e) => setEmail(e.target.value)}
-        margin="normal"
-        label="E-mail"
-        fullWidth
-        variant="outlined"
-      />
-      <TextField
-        onChange={(e) => setSenha(e.target.value)}
-        margin="normal"
-        label="Senha"
-        fullWidth
-        variant="outlined"
-      />
-      <TextField
-        onChange={(e) => setConfirmarSenha(e.target.value)}
-        margin="normal"
-        label="Confirmar Senha"
-        fullWidth
-        variant="outlined"
-      />
-      <Button onClick={onSubmit} variant="contained" color="primary">
-        Cadastrar
-      </Button>
+      <form onSubmit={onSubmit}>
+        <TextField
+          onChange={(e) => setName(e.target.value)}
+          margin="normal"
+          label="Nome"
+          required
+          fullWidth
+          variant="outlined"
+        />
+        <TextField
+          onChange={(e) => setEmail(e.target.value)}
+          margin="normal"
+          label="E-mail"
+          required
+          type="email"
+          fullWidth
+          variant="outlined"
+        />
+        <TextField
+          onChange={(e) => setPassword(e.target.value)}
+          margin="normal"
+          label="Senha"
+          type="password"
+          required
+          fullWidth
+          variant="outlined"
+        />
+        <TextField
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          margin="normal"
+          type="password"
+          required
+          label="Confirmar Senha"
+          fullWidth
+          variant="outlined"
+        />
+        <Button fullWidth type="submit" variant="contained" color="primary">
+          Cadastrar
+        </Button>
+      </form>
     </Container>
   );
 };
